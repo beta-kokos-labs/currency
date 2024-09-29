0c3bb5c95e9a4c5e56733a6313c64cc4098bc734
Ethan <175902687+kokorocks@users.noreply.github.com>
2024-09-29 16:16:12 -0400

    Update service.js

--- service/service.js
+++ service/service.js
@@ -22,7 +22,7 @@ let conn;
 
 peer.on('open', (id) => {
     console.log('My peer ID is: ' + id);
-    document.getElementById('inputField').value=id
+    document.getElementById('inputField').value=id//+' | | https://kokos-labs.github.io/currency/test.html?id='encodeURIComponent(id)+' to auto transfer'
     alert(`Your ID is: ${id} or go to this URL: https://kokos-labs.github.io/currency/test.html?id=${encodeURIComponent(id)} to auto transfer`);
 });
 
